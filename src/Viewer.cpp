55f01e5c89b134bb3798b0ac3957752b7413ade4
valche5 <valche5@hotmail.com>
2018-02-04 23:23:42 +0100

    Restore merge error ... need to be more carefull

--- src/Viewer.cpp
+++ src/Viewer.cpp
@@ -15,7 +15,7 @@ Viewer::Viewer(QWidget *parent)
   // Auto update opengl drawing
   QTimer* timer = new QTimer( this );
   connect(timer, &QTimer::timeout, this, &Viewer::updateScene);
-  timer->start(0);
+  timer->start(5);
 }
 
 Viewer::~Viewer(){
